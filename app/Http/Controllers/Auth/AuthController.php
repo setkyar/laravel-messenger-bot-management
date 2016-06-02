fa48159fc07e82d5b2c2aaf161781137d7b446e3
Set Kyar Wa Lar <setkyar16@gmail.com>
2016-06-02 17:38:51 +0630

    Add basic auth

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/add-bot';
 
     /**
      * Create a new authentication controller instance.
